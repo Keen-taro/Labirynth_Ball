9b834a16dc05895515032b3003de91a6594da34e
Keen[] <77962920+Keen-taro@users.noreply.github.com>
2023-05-09 07:38:56 +0700

    2nd Push
    Maze Generator + player in one of the maze node (random)

--- Assets/Assets/Script/MouseLook.cs
+++ Assets/Assets/Script/MouseLook.cs
@@ -20,7 +20,7 @@ public class MouseLook : MonoBehaviour
         float rotationY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         
         xRotation -= rotationY;
-        xRotation = Mathf.Clamp(xRotation, -90f , 75f);
+        xRotation = Mathf.Clamp(xRotation, -90f , 15f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f , 0f);
         playerBody.Rotate(Vector3.up * rotationX);
